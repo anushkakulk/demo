afcc2bc03fa299a93cc48a6a74cc65bc2180d386
0hWhen <heowen10@gmail.com>
2020-10-19 21:26:51 -0400

    commit changes

--- DemoLevel/Assets/MC/player_movement.cs
+++ DemoLevel/Assets/MC/player_movement.cs
@@ -10,12 +10,7 @@ public class player_movement : MonoBehaviour
     public float moveSpeed = 5f;
     
 
-    // Start is called before the first frame update
-    void Start()
     
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
