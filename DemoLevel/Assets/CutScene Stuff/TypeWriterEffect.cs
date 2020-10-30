f793f53e4d66aae580feeffd9eb0d3312fe837f8
FluffyChipmunk <66707110+FluffyChipmunk@users.noreply.github.com>
2020-10-29 22:54:23 -0400

    Added typewriter effect, starting dialogue system

---
+++ DemoLevel/Assets/CutScene Stuff/TypeWriterEffect.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypeWriterEffect : MonoBehaviour
+{
+
+    public float delay = 0.001f;
+    public string fullText;
+    private string currentText = "";
+    private bool isMC = true;
+    // Start is called before the first frame update
+    
+    
+    
+    void Start()
+    {
+        StartCoroutine(ShowText());
+    }
+
+    IEnumerator ShowText() 
+    {
+        
+        for (int i =0; i<fullText.Length; i++) 
+        {
+            currentText = fullText.Substring(0, i);
+            this.GetComponent<Text>().text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+
+    /*void textColor()
+    {
+        if (isMC)
+        {
+            GetComponent<ShowText>().color = Color.red;
+        }
+    }*/
+}
