377b4aa1eb8b67917b211d15a7c41632f2eaa466
anushka366 <67698204+anushka366@users.noreply.github.com>
2020-11-01 20:40:00 -0500

    added a health system w/ power ups
    start with 5 lives per level, lose a life anytime MC gets respawned, gain a life if health < 5 with power ups

--- DemoLevel/Assets/CutScene Stuff/RespawnScript.cs
+++ DemoLevel/Assets/CutScene Stuff/RespawnScript.cs
@@ -1,19 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+
 
 public class RespawnScript : MonoBehaviour
 {
+    
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
+
+
     
-    private void OnTriggerEnter2D(Collider2D other) 
-    { 
-        if(other.CompareTag("Player"))
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
         {
+           HealthScript.health -= 1;
             player.transform.position = new Vector3(respawnPoint.transform.position.x, respawnPoint.transform.position.y, player.transform.position.z);
             //player.transform.position = respawnPoint.transform.position;
             Physics.SyncTransforms();
+
         }
     }
 }
---
+++ DemoLevel/Assets/PowerUpScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpScript : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("PowerUp"))
+        {
+            Destroy(col.gameObject, .25f);
+            HealthScript.health += 1;
+        }
+
+    }
+}
