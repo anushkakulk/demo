8caccf36a56309b5a9318ee018905699f3e561fc
FluffyChipmunk <66707110+FluffyChipmunk@users.noreply.github.com>
2020-11-05 10:16:00 -0500

    Wroking on animation of opening scene

---
+++ DemoLevel/Assets/CutScene Stuff/OpeningAnimationTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class OpeningAnimationTrigger : MonoBehaviour
+{
+    public Vector3 target;
+    public float speed;
+    private Vector3 position;
+    public Animator MyAnimator;
+    // Use this for initialization
+    void Start()
+    {
+        MyAnimator.gameObject.GetComponent<Animator>().enabled = true;
+        position = gameObject.transform.position;
+        MyAnimator = GetComponent<Animator>();
+        target = new Vector3(6, -.6f, 0);
+    }
+
+    
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
+        if(position.y == target.y && position.x == target.x)
+        {
+            End();
+        }
+    }
+
+    void End()
+    {
+        MyAnimator.enabled = false ;
+    }
+
+}
