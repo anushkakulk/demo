b26bd9d45dfd3e47b26e950f044bd18007680a7f
AidanS519 <supertepig2022@gmail.com>
2020-12-03 09:18:21 -0500

    Revert "Merge branch 'main' of https://github.com/cswstonks/demo into main"
    This reverts commit 155cb80874e8db8475fdfaa466baeb38c5171f07, reversing
    changes made to e62a535cc1c1cec08e6852cbff921e0701d480bc.

--- DemoLevel/Assets/CutScene Stuff/RespawnScript.cs
+++ DemoLevel/Assets/CutScene Stuff/RespawnScript.cs
@@ -1,19 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+
 
 public class RespawnScript : MonoBehaviour
 {
+    public AudioSource oofSource;
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
-    
-    private void OnTriggerEnter2D(Collider2D other) 
-    { 
-        if(other.CompareTag("Player"))
+    private void Start()
+    {
+       oofSource = GetComponent<AudioSource>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
         {
+            ScoreScript.scoreValue -= 10;
+           HealthScript.health -= 1;
             player.transform.position = new Vector3(respawnPoint.transform.position.x, respawnPoint.transform.position.y, player.transform.position.z);
             //player.transform.position = respawnPoint.transform.position;
             Physics.SyncTransforms();
+            oofSource.Play();
+
         }
     }
 }
