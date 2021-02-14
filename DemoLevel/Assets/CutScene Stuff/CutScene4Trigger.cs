2d72c8ee0c008280ee091a0d740b2c2a5f3fd6b2
FluffyChipmunk <66707110+FluffyChipmunk@users.noreply.github.com>
2021-02-13 21:17:24 -0500

    "finished" game

---
+++ DemoLevel/Assets/CutScene Stuff/CutScene4Trigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutScene4Trigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueCutScene4>().StartDialogue(dialogue);
+    }
+
+    void Start()
+    {
+        TriggerDialogue();
+    }
+}
